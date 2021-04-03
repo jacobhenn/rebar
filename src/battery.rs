c4a5a451be997c56568fe3c085adb3175fc817f4
Jacob Henn <windowWeasel@outlook.com>
2021-04-03 14:44:06 -0700

    fix battery display

--- src/battery.rs
+++ src/battery.rs
@@ -5,7 +5,7 @@ use std::error::Error;
 async fn get_charge() -> Result<u8, Box<dyn Error>> {
     let contents = fs::read("/sys/class/power_supply/BAT0/charge_now").await?;
     let raw_charge: u32 = String::from_utf8_lossy(&contents).trim().parse()?;
-    Ok((raw_charge * (1 / 3487)) as u8)
+    Ok((raw_charge / 34870) as u8)
 }
 
 pub struct Battery(pub u8);
@@ -14,7 +14,12 @@ impl fmt::Display for Battery {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let Self(charge) = self;
         let charge_difference = 100 - charge;
-        write!(f, "^fg(#ebcb8b)^r({}x10)^r({}x10)^fg()", charge, charge_difference)
+        write!(
+            f,
+            "^fg(#ebcb8b)^r({}x10)^fg()^fg(#4c566a)^r({}x10)^fg()",
+            charge,
+            charge_difference
+        )
     }
 }
 
